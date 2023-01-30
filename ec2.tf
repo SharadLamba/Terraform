03ed260ce7b1f13b74cf2c96392dc1a97bf97751
SharadLamba <114917500+SharadLamba@users.noreply.github.com>
2023-01-30 16:50:59 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "terra_dev"{
    count = "1"
    ami = "ami-01a4f99c4ac11b03c"
-   instance_type = "t2.micro"
+   instance_type = "t2.medium"
    tags={
     Name = "terra-jenkins"
 }
